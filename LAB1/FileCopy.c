9931a9930f959c37c90bf66aab0f78b91703b488
David Eisenbaum <DavidJEisenbaum@Gmail.com>
2020-06-01 11:28:07 -0700

    Fixed folder names

--- LAB1/FileCopy.c
+++ LAB1/FileCopy.c
@@ -3,14 +3,18 @@
 int main(int argc, char* argv[]) {
     printf("Program name: %s", argv[0]);
     if(argc == 2) {
-        printf("\n Invalid input");
+        printf("\n Invalid input, not enough arguments\n");
     }
-    if(argc >= 3){
-        printf("\nNumber of CLI arguments: %d", argc);
+    else if(argc == 3){
+        printf("\nNumber of CLI arguments: %d\n", argc);
 
         for(int i = 0; i < argc; i++) {
             printf("\nargv[%d]: %s", i, argv[i]);
         }
     }
+    else {
+        printf("\nExceeded limit of arguments\n");
+    }
+    
     return 0;
 }
