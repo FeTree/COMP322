244de9e01cd19705233f490757421abf0f7eb471
David Eisenbaum <DavidJEisenbaum@Gmail.com>
2020-06-15 21:40:07 -0700

    done

--- LAB3/lab3.c
+++ LAB3/lab3.c
@@ -19,12 +19,33 @@ int lengthOfNumberList;
 void *findAverage() {
     int sum = 0;
     for(int i = 0; i < lengthOfNumberList ; i++){
-        printf("%d\n", numbers[i]);
         sum += numbers[i];
     }
 
     average = sum / lengthOfNumberList;
     return NULL;
+}
+
+void *findMax() {
+    int first = numbers[0];
+    for(int i = 0; i < lengthOfNumberList; i++){
+        if(numbers[i] > first) {
+            first = numbers[i];
+        }
+    }
+
+    maximum = first;
+}
+
+void *findMin() {
+    int first = numbers[0];
+    for(int i = 0; i < lengthOfNumberList; i++){
+        if(numbers[i] < first) {
+            first = numbers[i];
+        }
+    }
+
+    minimum = first;
 }
 
 int main() {
@@ -38,13 +59,34 @@ int main() {
         numbers[i] = rand() % 101;
     }
 
+    //Print array for users
+    printf("Generated numbers: ");
+    for(int i = 0; i < lengthOfNumberList; i++){
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
     // Thread 1
     pthread_t t_id_1;
-    printf("Before using threading\n");
     pthread_create(&t_id_1, NULL, findAverage, NULL);
     pthread_join(t_id_1, NULL);
 
+
+    // Thread 2
+    pthread_t t_id_2;
+    pthread_create(&t_id_2, NULL, findMax, NULL);
+    pthread_join(t_id_2, NULL);
+
+    // Thread 3
+    pthread_t t_id_3;
+    pthread_create(&t_id_3, NULL, findMin, NULL);
+    pthread_join(t_id_3, NULL);
+
+
+    // Print Results
     printf("Average: %d\n", average);
+    printf("Maximum: %d\n", maximum);
+    printf("Minimum: %d\n", minimum);
 
 
     exit(0);
